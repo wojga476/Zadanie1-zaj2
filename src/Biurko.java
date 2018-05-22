f8785b463990f40e081a5694ddebfa6ea6b3947c
Wojtek <wojga476@gmail.com>
2018-05-22 13:13:39 +0200

    Stworzenie klas Biurko i Wymiary

---
+++ src/Biurko.java
@@ -0,0 +1,12 @@
+public class Biurko {
+
+    int szerokosc;
+    int dlugosc;
+    int wysokosc;
+
+//    Biurko( int s, int d, int w){
+//        s = szerokosc;
+//        d = dlugosc;
+//        w = wysokosc;
+//    }
+}
---
+++ src/Wymiary.java
@@ -0,0 +1,23 @@
+public class Wymiary {
+    public static void main(String[] args) {
+
+        Biurko biurko1 = new Biurko();
+        biurko1.szerokosc = 40;
+        biurko1.dlugosc = 22;
+        biurko1.wysokosc = 15;
+        Biurko biurko2 = new Biurko();
+        biurko2.szerokosc = 36;
+        biurko2.dlugosc = 25;
+        biurko2.wysokosc = 17;
+
+        System.out.println(biurko1.szerokosc);
+        System.out.println(biurko1.dlugosc);
+        System.out.println(biurko1.wysokosc);
+        System.out.println(biurko2.szerokosc);
+        System.out.println(biurko2.dlugosc);
+        System.out.println(biurko2.wysokosc);
+
+
+
+    }
+}
