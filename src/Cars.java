e77837813eeb5bb2e4240e99605dbe471f11be46
Wojtek <wojga476@gmail.com>
2018-05-22 13:55:25 +0200

    Zamiana println na printf

--- src/Cars.java
+++ src/Cars.java
@@ -3,11 +3,11 @@ public class Cars {
         Car car1 = new Car("BMW", "M5", 2007, 2.6); //marka, model, rocznik, pojemność silnika
         Car car2 = new Car("Opel", "Vectra", 2004, 1.8);
 
-        System.out.println("Samochód 1:");
-        System.out.println("Marka: " + car1.brand + ", model: " + car1.model +
+        System.out.printf("Samochód 1:");
+        System.out.printf("\nMarka: " + car1.brand + ", model: " + car1.model +
                 ", rocznik: " + car1.year + ", pojemność: " + car1.capacity);
-        System.out.println("Samochód 2:");
-        System.out.println("Marka: " + car2.brand + ", model: " + car2.model +
+        System.out.printf("\nSamochód 2: ");
+        System.out.printf("\nMarka: " + car2.brand + ", model: " + car2.model +
                 ", rocznik: " + car2.year + ", pojemność: " + car2.capacity);
     }
 }
